d50d10111b5568aafe3d399d5cd9742ce7d45134
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-05-29 14:58:03 -0500

    Projects Created
    API is an ASP.NET Core Web API and SPA is an Angular Web Application that access the API created in the API project.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+        
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
