0174f2713b9b851105bf8610d13bdccf14b8e050
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-06-02 20:46:27 -0500

    End of Section 6

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -8,7 +8,9 @@ import {AppComponent} from './app.component';
 import {NavComponent} from './nav/nav.component';
 import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
-import {ErrorInterceptor} from './_services/error.interceptor';
+import {ErrorInterceptorProvider} from './_services/error.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -20,11 +22,13 @@ import {ErrorInterceptor} from './_services/error.interceptor';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
     AuthService,
-    ErrorInterceptor
+    ErrorInterceptorProvider
   ],
   bootstrap: [AppComponent]
 })
--- SPA/src/app/register/register.component.ts
+++ SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {AuthService} from '../_services/auth.service';
+import {AlertifyService} from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private alertify: AlertifyService) {
   }
 
   ngOnInit(): void {
@@ -18,14 +19,13 @@ export class RegisterComponent implements OnInit {
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('Registration Successful');
+      this.alertify.success('Registration Successful');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('Cancelled');
   }
 }
