7978a137706c0e62ba7c6a44cf87cd39031e9846
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-06-02 21:45:51 -0500

    End of Section 7

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -2,33 +2,44 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import {AuthService} from './_services/auth.service';
+import {RouterModule} from '@angular/router';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 import {NavComponent} from './nav/nav.component';
 import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
 import {ErrorInterceptorProvider} from './_services/error.interceptor';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {MemberListComponent} from './member-list/member-list.component';
+import {ListsComponent} from './lists/lists.component';
+import {MessagesComponent} from './messages/messages.component';
+import {AlertifyService} from './_services/alertify.service';
+import {AuthService} from './_services/auth.service';
+import {appRoutes} from './routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    AlertifyService
   ],
   bootstrap: [AppComponent]
 })
