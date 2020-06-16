cd81524ac0ace899f57f2841117a9ee56e2a1d1b
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-06-15 23:34:33 -0500

    Azure Deployment 2

--- API/Startup.cs
+++ API/Startup.cs
@@ -83,22 +83,21 @@ namespace API
             }
             else
             {
-                // app.UseExceptionHandler(builder =>
-                // {
-                //     builder.Run(async context =>
-                //     {
-                //         context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
-                //         var error = context.Features.Get<IExceptionHandlerFeature>();
-                //         if (error != null)
-                //         {
-                //             context.Response.AddApplicationError(error.Error.Message);
-                //             await context.Response.WriteAsync(error.Error.Message);
-                //         }
-                //     });
-                // });
+                app.UseExceptionHandler(builder =>
+                {
+                    builder.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
                 app.UseHsts();
             }
-            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
 
